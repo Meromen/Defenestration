c06f10bce160043b31e21437ddd966f27ad2d8fa
KaterinaD <Katya.helldevil@yandex.ru>
2019-11-15 02:01:48 +1000

    Game

---
+++ Игра/Platformer/Assets/Sсripts/Caamera.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Caamera : MonoBehaviour
+{
+
+    public float dumpling = 1.5f;
+    public Vector2 offset = new Vector2(2f, 1f);
+    bool isleft;
+    private int lastx;
+    private Transform player;
+
+    void Start()
+    {
+        offset = new Vector2(Mathf.Abs(offset.x), offset.y);
+        FindPlayer(isleft);
+
+    }
+
+    public void FindPlayer(bool playerIsleft )
+    {
+        player =GameObject.FindGameObjectWithTag("Player").transform;
+        lastx = Mathf.RoundToInt(player.position.x);
+        if (playerIsleft)
+        {
+            transform.position = new Vector3(player.position.x - offset.x, player.position.y - offset.y, transform.position.z);
+        }
+        else
+        {
+            transform.position = new Vector3(player.position.x + offset.x, player.position.y + offset.y, transform.position.z);
+        }
+    }
+
+    
+    
+    void Update()
+    {
+        if (player)
+        {
+            int currentX = Mathf.RoundToInt(player.position.x);
+            if (currentX > lastx) isleft = false;
+            else isleft |= currentX < lastx;
+            lastx = Mathf.RoundToInt(player.position.x);
+
+            Vector3 target;
+            if (isleft)
+            {
+                target = new Vector3(player.position.x - offset.x, player.position.y - offset.y, transform.position.z);
+            }
+
+        else
+            {
+                target = new Vector3(player.position.x + offset.x, player.position.y + offset.y, transform.position.z);
+            }
+            Vector3 currentPosition = Vector3.Lerp(transform.position, target, dumpling * Time.deltaTime);
+            transform.position = currentPosition;    
+
+        }
+    }
+}
---
+++ Игра/Platformer/Assets/Sсripts/Character1.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+
+
+public class Character1 : MonoBehaviour
+{
+    public int lives = 3;
+    public float speed = 4.0f;
+    public SpriteRenderer sprite;
+    private Rigidbody2D rb;
+
+     void Start()
+    {
+        rb = GetComponentInChildren <Rigidbody2D>();
+        sprite = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    void Move()
+    {
+        Vector3 temp = Vector3.right * Input.GetAxis("Horizontal");
+        transform.position = Vector3.MoveTowards(transform.position, transform.position+temp, speed*Time.deltaTime);
+
+        if (temp.x > 0)
+            sprite.flipX = true;
+        else
+            sprite.flipX = false;
+    }
+
+    void Jump()
+    {
+        rb.AddForce(transform.up * 7f, ForceMode2D.Impulse);
+    }
+
+  
+     void Update()
+    {
+
+ 
+        if(Input.GetButton("Horizontal"))
+        {
+            Move();
+        }
+        if ( Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
+}
+
